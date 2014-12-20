760f87783357b906d86d13f4a9b1c70978e051e5
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2014-12-20 17:09:54 +0100

    .

--- R/python.method.call.R
+++ R/python.method.call.R
@@ -2,7 +2,8 @@
 # CGB, 20100718
 #########################################################
 
-python.method.call <- function( py.object, py.method, ..., instance.name = instance.name ){
-    python.call( paste( py.object, py.method, sep = "." ), ..., instance.name = instance.name )
+python.method.call <- function( py.object, py.method, ..., instance.name = "" ){
+    i = instance.name
+    python.call( paste( py.object, py.method, sep = "." ), ..., instance.name=i )
 }
 
