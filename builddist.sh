584bf3471b3aebcdbafedafa11aeb2b3804e1bb5
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2014-12-21 12:34:12 +0100

    .

---
+++ builddist.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VERSION=`cat DESCRIPTION |grep Version|cut -f 2 -d " "`
+LIBNAME=rPithon
+
+CURDIR=`pwd`
+TMPDIR=`tempfile`
+rm -r $TMPDIR
+if ! mkdir $TMPDIR ; then
+	echo "Couldn't create temporary directory"
+	exit -1
+fi
+
+if ! hg archive $TMPDIR/${LIBNAME}-${VERSION} ; then
+	echo "Couldn't export repository"
+	exit -1
+fi
+
+cd $TMPDIR
+
+rm -f `find ${LIBNAME}-${VERSION} -name ".hg*"`
+rm ${LIBNAME}-${VERSION}/builddist.sh
+
+(cd inst/doc ; pdflatex rPithon.tex ; pdflatex rPithon.tex ; pdflatex rPithon.tex )
+	
+if ! tar cfz ${LIBNAME}-${VERSION}.tar.gz ${LIBNAME}-${VERSION}/ ; then
+	echo "Couldn't create archive"
+	exit -1
+fi
+
+mv $TMPDIR $CURDIR/
+
