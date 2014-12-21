17f763abd5fb7250581be365bf5897ca5483199d
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2014-12-21 12:35:36 +0100

    .

--- builddist.sh
+++ builddist.sh
@@ -20,8 +20,7 @@ cd $TMPDIR
 
 rm -f `find ${LIBNAME}-${VERSION} -name ".hg*"`
 rm ${LIBNAME}-${VERSION}/builddist.sh
-
-(cd inst/doc ; pdflatex rPithon.tex ; pdflatex rPithon.tex ; pdflatex rPithon.tex )
+(cd ${LIBNAME}-${VERSION}/inst/doc ; pdflatex rPithon.tex ; pdflatex rPithon.tex ; pdflatex rPithon.tex )
 	
 if ! tar cfz ${LIBNAME}-${VERSION}.tar.gz ${LIBNAME}-${VERSION}/ ; then
 	echo "Couldn't create archive"
