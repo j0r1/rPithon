0e82b8fa41bc46f140a2aa770fcb0975864b7fcd
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2014-12-24 12:24:07 +0100

    Better function, creates a new clone from mercurial

--- builddist.sh
+++ builddist.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-(cd package/ ; R CMD build . && mv rPithon*.tar.gz .. )
+CURDIR=`pwd`
+TMPDIR=`tempfile`
+rm -r $TMPDIR
+if ! mkdir $TMPDIR ; then
+	echo "Couldn't create temporary directory"
+	exit -1
+fi
+
+if ! hg archive $TMPDIR/ ; then
+	echo "Couldn't export repository"
+	exit -1
+fi
+
+( cd $TMPDIR/package/ ; R CMD build . && mv rPithon_*.tar.gz $CURDIR )
+rm -rf $TMPDIR
 
